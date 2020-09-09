12dc6d726e0526c48835cf68ee3d8d202e3f35dc
Nurbek Aidarken <nurbek0144@gmail.com>
2020-09-09 19:36:52 +0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {HeaderComponent} from './header/header.component';
+import {ListComponent} from './lists/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    HeaderComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
